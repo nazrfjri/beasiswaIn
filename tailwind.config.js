77cd71e9b966721fca4045760662eed62354b1b5
DESKTOP-IGVQADI\Asus <azelfa65@gmail.com>
2024-05-13 16:37:16 +0700

    config tailwind & favicon update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      container : {
+        center: true,
+        padding: '16px',
+      },
+      extend: {
+        backgroundImage : {
+          'parallax' : 'url("https://images.pexels.com/photos/176851/pexels-photo-176851.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")'
+        },
+        colors : {
+          primary: '#9333ea',
+          dark: '#0f172a',
+          secondary: '#64748b'
+        },
+        screens: {
+          '2xl': '1320px',
+        }
+      },
     },
     plugins: [],
   }
