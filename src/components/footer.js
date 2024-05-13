e704de0b865189acb710a4cfb0482645daac86bb
DESKTOP-IGVQADI\Asus <azelfa65@gmail.com>
2024-05-13 15:42:15 +0700

    install react

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function footer(){
+    return(
+        <>
+            <div>
+                <h1>Footer</h1>
+            </div>
+        </>
+    );
+};
+
+export default footer;
---
+++ src/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './components/homepage';
+
+function Home() {
+    return(
+        <>
+            <HomePage />
+        </>
+    );
+};
+
+export default Home;
